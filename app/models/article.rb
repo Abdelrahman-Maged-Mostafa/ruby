eca206ec0677a660599f264899313bf18144fc60
Abdelrahman-Maged-Mostafa <podapoda_poda@yahoo.com>
2025-08-13 14:39:17 +0300

    poda

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,4 @@ class Article < ApplicationRecord
     validates :title, presence:true ,length:{minimum:8,maximum:100}
     validates :description, presence:true,length:{minimum:30,maximum:250}
 end
-#video 18 in part 4 rednder errors
+#video 5 in part 5 rednder errors
