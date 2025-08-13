b43a7f277a285f63cba967aaa3360b8d5f637626
Abdelrahman-Maged-Mostafa <podapoda_poda@yahoo.com>
2025-08-13 15:31:42 +0300

    poda

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
 
   # Eager load code on boot for better performance and memory savings (ignored by Rake tasks).
   config.eager_load = true
-  config.assets.compile = true
+  config.assets.compile = false
   config.require_master_key = true
   # Full error reports are disabled.
   config.consider_all_requests_local = false
