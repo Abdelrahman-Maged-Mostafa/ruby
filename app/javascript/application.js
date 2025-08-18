50031ae1cc12947df218117c35d334fa2e73b8c2
Abdelrahman-Maged-Mostafa <podapoda_poda@yahoo.com>
2025-08-18 14:49:41 +0300

    poda

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@rails/ujs";
+import Rails from "@rails/ujs";
 
 Rails.start();
+
+console.log("JavaScript is working!");
