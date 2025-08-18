9e0cd542d7469e02429b36cde1005d7c7bf0e2f4
Abdelrahman-Maged-Mostafa <podapoda_poda@yahoo.com>
2025-08-18 14:17:30 +0300

    poda

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,4 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
-pin "@rails/ujs", to: "@rails--ujs.js" # @7.1.3
-pin "bootstrap" # @5.3.7
-pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
+pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.1.3/lib/assets/compiled/rails-ujs.js"
